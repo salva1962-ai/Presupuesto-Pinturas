bcddab9b1f3ea5ad28100f8e4d9d4fa870356482
Salvador Muñoz Portillo <salvador.munoz@masvoip.com>
2026-02-18 11:42:50 +0000

    Convertir PintaPro en PWA: Añadido manifest.json, sw.js y registro de service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'pintapro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar el Service Worker y cachear activos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activar y limpiar caches antiguos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estrategia: Cache First, then Network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
